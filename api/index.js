36896f5e2a27ff0ddf2cbe2f26d48a558c5ef367
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-20 06:58:24 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const events = require("./events");
 const news = require("./news");
 const theater = require("./theater");
 const theaterDetail = require("./theater-detail");
+const memberDetail = require("./member-detail");
 const newsDetail = require("./news-detail");
 const otherEndpoint = require("./other-endpoint");
 
@@ -20,6 +21,7 @@ app.use("/api/news", news);
 app.use("/api/news/", newsDetail);
 app.use("/api/theater", theater);
 app.use("/api/theater/", theaterDetail);
+app.use("/api/member/", memberDetail);
 app.use("/", otherEndpoint);
 
 // Menjalankan server
