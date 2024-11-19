f3a1278145bbb1f8463b42eb0d40e5dc27cb433c
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-20 06:49:34 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 // Mengimpor endpoint lainnya
 const events = require("./events");
+const news = require("./news");
 const theater = require("./theater");
 const theaterDetail = require("./theater-detail");
 const otherEndpoint = require("./other-endpoint");
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Menggunakan endpoint yang diimpor
 app.use("/api/events", events);
+app.use("/api/news", news);
 app.use("/api/theater", theater);
 app.use("/api/theater/", theaterDetail);
 app.use("/", otherEndpoint);
