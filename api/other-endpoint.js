2b6d7d6592de9137150bff7079a3f07b98ac58d0
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-20 07:21:23 +0700

    Update other-endpoint.js

--- api/other-endpoint.js
+++ api/other-endpoint.js
@@ -2,11 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-// Contoh endpoint lainnya
+// Endpoint untuk welcome message
 router.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "Ini adalah endpoint lainnya.",
+    message: "Selamat datang di API JKT48Connect! Ini adalah endpoint untuk website live.valzyofc.my.id.",
+    owner: "Valzy",
+    developer: "Valzyy",
+    social_media: {
+      instagram: "@valzy._",
+      tiktok: "@valzyycans",
+      youtube: "@valzyofc"
+    },
   });
 });
 
