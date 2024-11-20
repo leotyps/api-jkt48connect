49d2b4b2f7167a6f5dcfcf7d0a9cb54e31c5c92b
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-20 15:51:22 +0700

    Create dateParser.js

---
+++ helpers/dateParser.js
@@ -0,0 +1,10 @@
+function parseCustomDate(dateString) {
+  // Contoh format input: "20/11/2024/14:30"
+  const [day, month, year, time] = dateString.split("/");
+  const [hour, minute] = time.split(":");
+
+  // Buat objek Date berdasarkan komponen-komponen tersebut
+  return new Date(year, month - 1, day, hour, minute);
+}
+
+module.exports = parseCustomDate;
