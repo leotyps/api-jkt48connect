981d108b381a2c943f1c8a6cc77e1352d614677f
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-21 05:42:56 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ const newsDetail = require("./news-detail");
 const otherEndpoint = require("./other-endpoint");
 const nowlive = require("./now-live");
 const checkApikey = require("./check-apikey");
+const genApi = require("./generate-apikey");
 
 // Middleware untuk parsing JSON
 app.use(express.json());
@@ -27,6 +28,7 @@ app.use("/api/member/", memberDetail);
 app.use("/api/live", nowlive);
 app.use("/", otherEndpoint);
 app.use("/api", checkApikey);
+app.use("/api", genApi);
 
 // Menjalankan server
 app.listen(PORT, () => {
