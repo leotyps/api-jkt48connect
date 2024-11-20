f14ecfeb8dbd38dfc312402815fff9648587f9d5
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-20 15:46:56 +0700

    Create apiKeys.js

---
+++ apiKeys.js
@@ -0,0 +1,6 @@
+const apiKeys = {
+  "VALZY": new Date("2024-12-31T23:59:59Z"), // API key dengan tanggal kedaluwarsa
+  "67890-FGHIJ": new Date("2024-11-30T23:59:59Z"),
+};
+
+module.exports = apiKeys;
