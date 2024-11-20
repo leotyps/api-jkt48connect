848063b10c3a0623eb0ef42c17f277bb4af8a8d1
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-20 18:14:06 +0700

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -2,7 +2,8 @@ const parseCustomDate = require("./helpers/dateParser");
 
 const apiKeys = {
   "ADELINE": parseCustomDate("31/12/2029/23:59"), // API key kedaluwarsa 31 Desember 2024 pukul 23:59
-  "67890-FGHIJ": parseCustomDate("30/11/2024/12:00"), // API key kedaluwarsa 30 November 2024 pukul 12:00
-};
+  "67890-FGHIJ": parseCustomDate("30/11/2024/12:00"),
+  "ForxFyyre": parseCustomDate("21/11/2024/18:15"),
+ };
 
 module.exports = apiKeys;
