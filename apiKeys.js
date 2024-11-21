6e2fee63b4bb7f883951f6628f50692e2081fdee
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-21 17:50:32 +0700

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -1,11 +1,11 @@
 const parseCustomDate = require("./helpers/dateParser");
 
 const apiKeys = {
-  ADELINE: {
-    expiryDate: parseCustomDate("31/12/2029/23:59"),
-    remainingRequests: 100, // Jumlah request tersisa untuk hari ini
-    maxRequests: 100, // Limit maksimum per hari
-    lastAccessDate: "2024-11-20", // Tanggal terakhir akses (format: YYYY-MM-DD)
+  01092008: {
+    expiryDate: "unli", // Tidak terbatas
+    remainingRequests: "∞", // Tidak terbatas
+    maxRequests: "∞", // Tidak terbatas
+    lastAccessDate: "2024-11-20",
   },
   "67890-FGHIJ": {
     expiryDate: parseCustomDate("30/11/2024/12:00"),
@@ -14,7 +14,7 @@ const apiKeys = {
     lastAccessDate: "2024-11-20",
   },
   "ForxFyyre": {
-    expiryDate: "unli", // Tidak terbatas
+    expiryDate: parseCustomDate("21/11/2024/18:15"),
     remainingRequests: "∞", // Tidak terbatas
     maxRequests: "∞", // Tidak terbatas
     lastAccessDate: "2024-11-20",
