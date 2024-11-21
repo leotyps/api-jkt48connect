01ff5d8ec26574ef4624d3458216972935b4f993
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-21 17:42:38 +0700

    Update dateParser.js

--- helpers/dateParser.js
+++ helpers/dateParser.js
@@ -1,10 +1,10 @@
 function parseCustomDate(dateString) {
-  // Jika input adalah "-" (tak terbatas), kembalikan null (menandakan tidak ada tanggal kedaluwarsa)
-  if (dateString === "-") {
-    return null;
+  // Jika tanggal adalah 'unli' atau '-'
+  if (dateString === "unli" || dateString === "-") {
+    return dateString; // Kembalikan string "unli" atau "-" untuk menandakan tidak terbatas
   }
 
-  // Contoh format input: "20/11/2024/14:30"
+  // Jika formatnya adalah tanggal dan waktu
   const [day, month, year, time] = dateString.split("/");
   const [hour, minute] = time.split(":");
 
