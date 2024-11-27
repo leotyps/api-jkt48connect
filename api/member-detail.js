f94bb25a34345b62339062c4d005756dba59ab05
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-27 14:04:54 +0700

    Update member-detail.js

--- api/member-detail.js
+++ api/member-detail.js
@@ -2,12 +2,13 @@ const express = require("express");
 const axios = require("axios");
 const cors = require('cors');
 const validateApiKey = require("../middleware/auth"); // Import middleware validasi API key
-
+const app = express();
 const router = express.Router();
+
 
 // Enable CORS for all domains (or specific domains)
 app.use(cors({
-  origin: '*', // Atau set domain spesifik, misalnya 'https://yourfrontenddomain.com'
+  origin: '*', 
 }));
 
 
