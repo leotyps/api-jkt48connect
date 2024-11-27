d0a5e4d5a0b3595887da50377c13d8368fe24b0c
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-27 18:35:28 +0700

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -7,6 +7,24 @@ const apiKeys = {
     maxRequests: "∞", // Tidak terbatas
     lastAccessDate: "2024-11-20",
   },
+  "J48-9F2A7B1D": {
+    expiryDate: "unli",
+    remainingRequests: 50,
+    maxRequests: 50,
+    lastAccessDate: "2024-11-20",
+  },
+  "JKT-4F5C3D8A": {
+    expiryDate: "unli",
+    remainingRequests: 50,
+    maxRequests: 50,
+    lastAccessDate: "2024-11-20",
+  },
+  "J48-2E9D4B7C": {
+    expiryDate: "unli",
+    remainingRequests: 50,
+    maxRequests: 10,
+    lastAccessDate: "2024-11-20",
+  },
   "67890-FGHIJ": {
     expiryDate: parseCustomDate("30/11/2024/12:00"),
     remainingRequests: 10,
