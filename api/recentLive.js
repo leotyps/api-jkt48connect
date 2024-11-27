bdc279cad9d5aa0f7ac2316a4d1bccef1058aeee
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-27 14:03:39 +0700

    Update recentLive.js

--- api/recentLive.js
+++ api/recentLive.js
@@ -2,14 +2,13 @@ const express = require("express");
 const axios = require("axios");
 const cors = require('cors');
 const validateApiKey = require("../middleware/auth"); // Import middleware validasi API key
-
+const app = express();
 const router = express.Router();
 
 
 // Enable CORS for all domains (or specific domains)
-// Enable CORS for all domains (or specific domains)
 app.use(cors({
-  origin: '*', // Atau set domain spesifik, misalnya 'https://yourfrontenddomain.com'
+  origin: '*', 
 }));
 
 // Endpoint untuk mengambil data live berdasarkan ID
