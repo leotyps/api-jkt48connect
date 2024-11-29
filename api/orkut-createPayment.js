08d14804b64180f55c12f7d7cdd82eb763151841
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-29 13:09:07 +0700

    Update orkut-createPayment.js

--- api/orkut-createPayment.js
+++ api/orkut-createPayment.js
@@ -26,7 +26,7 @@ router.get("/", validateApiKey, async (req, res) => {
     const response = await axios.get(`https://apiv2.abidev.tech/api/orkut/createpayment?amount=${amount}&codeqr=${codeqr}&logostore=${logostore}`);
     const paymentData = response.data;
 
-    // Menghapus data 'author' dari respons
+    // Menghapus data 'creator' dari respons
     delete paymentData.creator;
 
     // Mengembalikan data dalam format JSON langsung tanpa author
