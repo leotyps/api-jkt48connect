2335a2c30032d92acfefea658b724b2b51982aee
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-12-05 20:07:41 +0700

    Update apiKey.js

--- models/apiKey.js
+++ models/apiKey.js
@@ -1,14 +1,28 @@
-// models/apiKey.js
 const mongoose = require("mongoose");
 
+// Membuat schema untuk API Key
 const apiKeySchema = new mongoose.Schema({
   key: { type: String, required: true, unique: true },
-  expiryDate: { type: Date, required: true },
-  remainingRequests: { type: Number, required: true },
-  maxRequests: { type: Number, required: true },
+  expiryDate: { type: String, required: true },  // Menggunakan String untuk menangani 'unli' dan tanggal
+  remainingRequests: { 
+    type: mongoose.Schema.Types.Mixed,  // Menggunakan Mixed agar bisa menangani angka atau '∞'
+    required: true 
+  },
+  maxRequests: { 
+    type: mongoose.Schema.Types.Mixed,  // Menggunakan Mixed agar bisa menangani angka atau '∞'
+    required: true 
+  },
   lastAccessDate: { type: Date, required: true },
 });
 
+// Menambahkan method untuk memperbarui remainingRequests
+apiKeySchema.methods.updateRemainingRequests = function() {
+  if (this.remainingRequests !== "∞") {
+    this.remainingRequests -= 1;
+  }
+};
+
+// Membuat model ApiKey berdasarkan schema yang sudah dibuat
 const ApiKey = mongoose.model("ApiKey", apiKeySchema);
 
 module.exports = ApiKey;
