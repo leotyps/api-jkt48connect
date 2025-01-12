cb28bb307479f7f5d0d535bffefe8d2d0d1ed149
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2025-01-12 11:49:45 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,21 +1,19 @@
-const { Pool } = require("pg"); // Menggunakan Pooling untuk koneksi database
+const { Client } = require("pg"); // Import PostgreSQL client
 
-// Konfigurasi koneksi ke CockroachDB dengan Pooling
-const pool = new Pool({
+// Konfigurasi koneksi ke CockroachDB
+const client = new Client({
   user: "jkt48connect_apikey", // Gantilah dengan username Anda
   host: "jkt48connect-7018.j77.aws-ap-southeast-1.cockroachlabs.cloud",
   database: "defaultdb",
   password: "vAgy5JNXz4woO46g8fho4g", // Gantilah dengan password Anda
   port: 26257,
   ssl: { rejectUnauthorized: false },
-  connectionTimeoutMillis: 10000, // Timeout koneksi 5 detik
-  statement_timeout: 10000, // Timeout query 5 detik
 });
 
-// Fungsi untuk membuka koneksi ke CockroachDB (pooling akan menangani koneksi)
+// Fungsi untuk membuka koneksi ke CockroachDB
 async function connectDb() {
   try {
-    await pool.connect();
+    await client.connect();
     console.log("Koneksi ke CockroachDB berhasil");
   } catch (error) {
     console.error("Gagal terkoneksi ke CockroachDB:", error);
@@ -25,17 +23,17 @@ async function connectDb() {
 // Fungsi untuk menutup koneksi ke CockroachDB
 async function disconnectDb() {
   try {
-    await pool.end(); // Menghentikan semua koneksi dalam pool
+    await client.end();
     console.log("Koneksi ke CockroachDB ditutup");
   } catch (error) {
     console.error("Gagal menutup koneksi ke CockroachDB:", error);
   }
 }
 
-// Fungsi untuk menjalankan query terhadap CockroachDB dengan timeout
+// Fungsi untuk menjalankan query terhadap CockroachDB
 async function query(text, params) {
   try {
-    const result = await pool.query(text, params);
+    const result = await client.query(text, params);
     return result;
   } catch (error) {
     console.error("Error menjalankan query:", error);
@@ -43,9 +41,9 @@ async function query(text, params) {
   }
 }
 
-// Export pool, connectDb, disconnectDb, dan query agar bisa digunakan di file lain
+// Export client, connectDb, disconnectDb, dan query agar bisa digunakan di file lain
 module.exports = {
-  pool,
+  client,
   connectDb,
   disconnectDb,
   query,
