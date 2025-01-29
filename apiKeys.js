4229bb35b432cb351bd0f06fcd0b06a2232e925d
Valzyy <jkt48connect@gmail.com>
2025-01-29 11:19:05 +0700

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -16,6 +16,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2024-11-20",
   },
+  "liaa123": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2024-11-20",
+  },
   "J48-Mami": {
     expiryDate: "unli",
     remainingRequests: 250,
