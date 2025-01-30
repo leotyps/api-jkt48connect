6ddd0a9aa3b6f3b0eec0a9ffacb37a761084d59b
Valzyy <jkt48connect@gmail.com>
2025-01-30 18:06:31 +0700

    Update qr.js

--- helpers/qr.js
+++ helpers/qr.js
@@ -3,23 +3,28 @@ const axios = require("axios");
 const FormData = require("form-data");
 const { v4: uuidv4 } = require("uuid"); // Menggunakan UUID untuk memastikan nama file unik
 
-// Fungsi untuk mengunggah buffer ke Pomf2
-async function pomf(media) {
+// Fungsi untuk mengunggah buffer ke tmpfiles.org
+async function tmpFiles(buffer) {
   return new Promise(async (resolve, reject) => {
-    const formData = new FormData();
-    formData.append('files[]', media, { 
-      filename: new Date() * 1 + '.jpg' // Nama file menggunakan timestamp agar unik
+    // Menentukan ekstensi dan tipe MIME secara manual berdasarkan format buffer (JPEG)
+    const ext = 'jpg';
+    const mime = 'image/jpeg';
+
+    const form = new FormData();
+    form.append('file', buffer, {
+      filename: new Date() * 1 + '.' + ext,
+      contentType: mime
     });
 
     try {
-      const response = await axios.post('https://pomf2.lain.la/upload.php', formData, {
+      const { data } = await axios.post("https://tmpfiles.org/api/v1/upload", form, {
         headers: {
-          ...formData.getHeaders(),
+          ...form.getHeaders(),
         },
       });
-      resolve(response.data); // Mengembalikan data dari respons Pomf
-    } catch (error) {
-      reject(error?.response || error); // Menangani error jika terjadi
+      resolve(data);
+    } catch (e) {
+      reject(e?.response || e);
     }
   });
 }
@@ -29,8 +34,8 @@ const createQr = async (text) => {
   try {
     // Menggunakan UUID untuk memastikan nama file unik
     const buffer = await qrcode.toBuffer(text, { width: 1020 });
-    const urlData = await pomf(buffer); // Mengunggah buffer ke Pomf
-    return { url: urlData?.files[0]?.url }; // Mengembalikan URL file yang diunggah
+    const data = await tmpFiles(buffer); // Menggunakan tmpFiles untuk upload buffer
+    return { url: data.url }; // Menyimpan URL dari tmpfiles
   } catch (err) {
     console.error("Error creating QR code:", err);
     throw err;
