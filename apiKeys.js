9bc42230d73d471e6fe53abc995ef8c4b5d6a9e1
Valzyy <jkt48connect@gmail.com>
2025-03-30 19:22:11 +0700

    Update API key: testdoang

--- apiKeys.js
+++ apiKeys.js
@@ -227,5 +227,14 @@ const apiKeys = {
     premium: true,
     seller: true
   },
+
+  "testdoang": {
+    expiryDate: "unli",
+    remainingRequests: 10,
+    maxRequests: 250,
+    lastAccessDate: "2025-03-30",
+    premium: true,
+    seller: true
+  },
 };
 module.exports = apiKeys;
