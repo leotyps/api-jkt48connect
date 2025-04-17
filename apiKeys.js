3cd1e95f248a87bd9a9eb576bad549ad19613060
Valzyy <jkt48connect@gmail.com>
2025-04-17 21:44:26 +0700

    Update API key: SSHJKS

--- apiKeys.js
+++ apiKeys.js
@@ -375,5 +375,12 @@ const apiKeys = {
     maxRequests: 30,
     lastAccessDate: "2025-04-16"
   },
+
+  "SSHJKS": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-17"
+  },
 };
 module.exports = apiKeys;
