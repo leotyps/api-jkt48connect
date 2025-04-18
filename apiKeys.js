eebd5b2224efdf8a37250ea085a8e099907cd3c2
Valzyy <jkt48connect@gmail.com>
2025-04-18 14:20:27 +0700

    Update API key: $

--- apiKeys.js
+++ apiKeys.js
@@ -445,5 +445,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2025-04-18"
   },
+
+  "$": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-18"
+  },
 };
 module.exports = apiKeys;
