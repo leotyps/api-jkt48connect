c83104ac7fcefae5a1d0e7829aa6e85e3091a313
Valzyy <jkt48connect@gmail.com>
2025-04-18 14:09:27 +0700

    Update API key: DL121848

--- apiKeys.js
+++ apiKeys.js
@@ -382,5 +382,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2025-04-17"
   },
+
+  "DL121848": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-18"
+  },
 };
 module.exports = apiKeys;
