545c3f00eb5d2eee1b0107a33dc73610fc96318c
Valzyy <jkt48connect@gmail.com>
2025-04-18 14:11:13 +0700

    Update API key: HELNAH

--- apiKeys.js
+++ apiKeys.js
@@ -403,5 +403,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2025-04-18"
   },
+
+  "HELNAH": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-18"
+  },
 };
 module.exports = apiKeys;
