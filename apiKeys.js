bdd6018b719a49181e98cb605861c0691c9a7018
Valzyy <jkt48connect@gmail.com>
2025-04-19 21:00:56 +0700

    Update API key: CL814785

--- apiKeys.js
+++ apiKeys.js
@@ -537,5 +537,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2025-04-19"
   },
+
+  "CL814785": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-19"
+  },
 };
 module.exports = apiKeys;
