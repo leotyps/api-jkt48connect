5e6fbfc00311489a6f453c71d2f62ab5a53d4c39
Valzyy <jkt48connect@gmail.com>
2025-04-19 19:43:04 +0700

    Update API key: CL2237812

--- apiKeys.js
+++ apiKeys.js
@@ -509,5 +509,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2025-04-19"
   },
+
+  "CL2237812": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-19"
+  },
 };
 module.exports = apiKeys;
