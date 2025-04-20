cd46b10a1abda86192b1e34a48cd044903bc6c99
Valzyy <jkt48connect@gmail.com>
2025-04-20 20:05:01 +0700

    Update API key: TEST67

--- apiKeys.js
+++ apiKeys.js
@@ -565,5 +565,12 @@ const apiKeys = {
     maxRequests: 250,
     lastAccessDate: "2025-04-20"
   },
+
+  "TEST67": {
+    expiryDate: "unli",
+    remainingRequests: 250,
+    maxRequests: 250,
+    lastAccessDate: "2025-04-20"
+  },
 };
 module.exports = apiKeys;
