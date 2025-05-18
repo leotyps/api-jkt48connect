e60eda3e5eba2d67862a3667ab713f9c4ba16ae2
leotyps <113692736+leotyps@users.noreply.github.com>
2025-05-18 20:24:06 +0700

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -79,6 +79,7 @@ const apiKeys = {
     remainingRequests: "∞", // Tidak terbatas
     maxRequests: "∞", // Tidak terbatas
     lastAccessDate: "2024-11-20",
+    premium: true
   },
   "manuel": {
     expiryDate: "unli",
